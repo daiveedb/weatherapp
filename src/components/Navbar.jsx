ccdeae2706fe5079b02a9b1bc45175fb5c02e45f
David Bajomo <bajomodavid18@gmail.com>
2023-07-18 20:19:00 +0100

    initial setup and routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='bg-purple-600 text-w w-full h-max px-4 py-6 flex justify-between items-center'>
+            <h1 className='text-2xl font-semibold text-white'>Cloud Nine</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/cityWeatherDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cityWeatherDetails = () => {
+  return (
+    <div>cityWeatherDetails</div>
+  )
+}
+
+export default cityWeatherDetails
