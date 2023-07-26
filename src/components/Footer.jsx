79ba170dd78de9add253890d40f68ad0ca4ae968
David Bajomo <bajomodavid18@gmail.com>
2023-07-26 21:24:33 +0100

    added info to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='fixed bottom-0 left-0 w-full p-5 flex justify-between items-center text-[#40434bff]  font-quickSand bg-[beige]'>
-        <p className='text-xs sm:text-sm md:text-base'>Powered By Openweathermap, GeoDB Cities, UnsplashAPI</p>
+        <p className='text-xs sm:text-sm md:text-base'>Powered By Openweathermap, GeoDB Cities, UnsplashAPI, Lottie Animations</p>
         <p className='text-xs sm:text-sm md:text-base'>Bajomo David &copy;</p>
     </div>
   )
